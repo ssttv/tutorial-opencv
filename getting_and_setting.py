8cf2a35a9a403bddfc87f91a7e4f8aadf11d1df2
Sergey Titov <sstmail@mail.ru>
2018-11-13 23:50:40 +0300

    feat: show a portion of the image

--- getting_and_setting.py
+++ getting_and_setting.py
@@ -17,3 +17,11 @@ image[0, 0] = (0, 0, 255)
 (b, g, r) = image[0, 0]
 print("New values of the pixel at (0, 0) - Red: {}, Green: {}, Blue: {}".format(r,
                                                               g, b))
+
+corner = image[0:100, 0:100]
+cv2.imshow("Corner", corner)
+
+image[0:100, 0:100] = (0, 255, 0)
+
+cv2.imshow("Updated", image)
+cv2.waitKey(0)
