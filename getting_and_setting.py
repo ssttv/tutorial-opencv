de37833619b19b4cc633adaded102232a7069d13
Sergey Titov <sstmail@mail.ru>
2018-11-13 22:35:43 +0300

    chore: correct argument naming

---
+++ getting_and_setting.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="Path to the image")
+args = vars(ap.parse_args())
