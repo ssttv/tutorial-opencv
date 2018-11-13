b3bc8b2a6b50727a3bb9f9c5dff3b524f9ca35fb
Sergey Titov <sstmail@mail.ru>
2018-11-13 22:38:22 +0300

    feat: show image on command from path arguments

--- getting_and_setting.py
+++ getting_and_setting.py
@@ -6,3 +6,6 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
                 help="Path to the image")
 args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
