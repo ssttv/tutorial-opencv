f059067ad14dd2a0d38efcdc4efe512e4a2c89eb
Sergey Titov <sstmail@mail.ru>
2018-11-13 22:45:22 +0300

    feat: remove junk line

--- getting_and_setting.py
+++ getting_and_setting.py
@@ -5,7 +5,6 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
                 help="Path to the image")
-args = vars(ap.parse_args())
 
-image = cv2.imread(args["image"])
+image = cv2.imread(vars(ap.parse_args())["image"])
 cv2.imshow("Original", image)
