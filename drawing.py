2c43ff16bb8b7aa3b706cf45f791f83265e371b6
Sergey Titov <sstmail@mail.ru>
2018-11-15 01:22:02 +0300

    feat: draw first rectangle

--- drawing.py
+++ drawing.py
@@ -1,14 +1,18 @@
 import numpy as np
 import cv2
 
-canvas = np.zeros((400,4,3), dtype="unit8")
+canvas = np.zeros((300,300,3), dtype="uint8")
 
 green = (0, 255, 0)
 cv2.line(canvas,(0,0), (300,300), green)
 cv2.imshow("Canvas", canvas)
-cv2.waitkey(0)
+cv2.waitKey(0)
 
 red = (0, 0, 255)
-cv2.line(canvas,(300,0), (169,300), red)
+cv2.line(canvas,(300,0), (0,300), red, 6)
 cv2.imshow("Canvas", canvas)
-cv2.waitkey(0)
+cv2.waitKey(0)
+
+cv2.rectangle(canvas, (20,20), (80,80), green, 5)
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
