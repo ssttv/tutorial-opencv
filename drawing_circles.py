5b50c9b0d4d67e3e320dd4ea2d0fd8621ccc11af
Sergey Titov <sstmail@mail.ru>
2018-11-15 02:34:50 +0300

    feat: draw some circles

---
+++ drawing_circles.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+canvas = np.zeros((300, 300, 3), dtype="uint8")
+(centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
+white = (255, 255, 255)
+
+for r in range(0, 175, 25):
+    cv2.circle(canvas. (centerX, centerY), r, white)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
