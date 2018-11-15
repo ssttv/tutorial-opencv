e6da3e5c520e890f7349b70e9eaf235911ef3b01
Sergey Titov <sstmail@mail.ru>
2018-11-15 05:09:00 +0300

    feat: draw random circles

--- drawing_circles.py
+++ drawing_circles.py
@@ -10,3 +10,13 @@ for r in range(0, 175, 25):
 
 cv2.imshow("Canvas", canvas)
 cv2.waitKey(0)
+
+for i in range(0, 25):
+    radius = np.random.randint(5, 200)
+    color = np.random.randint(0, 256, size=(3,)).tolist()
+    pt = np.random.randint(0, 300, size=(2,))
+
+    cv2.circle(canvas, tuple(pt), radius, color, -1)
+
+cv2.imshow("Canvas", canvas)
+cv2.waitKey()
