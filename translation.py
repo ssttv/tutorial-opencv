425453267bfe6a8842de5cfd18ff122412adaf50
Sergey Titov <sstmail@mail.ru>
2018-11-16 05:38:14 +0300

    feat: shift image down and right

---
+++ translation.py
@@ -0,0 +1,16 @@
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+                help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imnread(args["image"])
+cv2.imshow("Original", image)
+
+M = np.float32([[1, 0, 25], [0, 1, 50]])
+shifted = cv2.warpAffine(image, M, (image.sape[1], image.shape[0]))
+cv2.imshow("Shifted Down-Right", shifted)
