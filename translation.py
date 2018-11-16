0e6e0915f10bede644bf92ceb41929960b7b73bd
Sergey Titov <sstmail@mail.ru>
2018-11-16 07:38:00 +0300

    feat: use ergonomic translation functions

--- translation.py
+++ translation.py
@@ -8,13 +8,11 @@ ap.add_argument("-i", "--image", required = True,
                 help = "Path to the image")
 args = vars(ap.parse_args())
 
-image = cv2.imnread(args["image"])
+image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
 
-M = np.float32([[1, 0, 25], [0, 1, 50]])
-shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+shifted = imutils.translate(image, 40, 50)
 cv2.imshow("Shifted Down-Right", shifted)
 
-M = np.float32([[1, 0, -50], [0, 1, -90]])
-shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+shifted = imutils.translate(image, -50, -25)
 cv2.imshow("Shifted Up-Left", shifted)
