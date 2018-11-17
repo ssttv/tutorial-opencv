26eba5359b2ade00af4e460ead7fc087577e4262
Sergey Titov <sstmail@mail.ru>
2018-11-17 14:29:14 +0300

    feat: resize via imutils function

--- resize.py
+++ resize.py
@@ -8,7 +8,7 @@ ap.add_argument("-i", "--image", required = True, help = "Path to the image")
 args = vars(ap.parse_args())
 
 image = cv2.imread(args["image"])
-cv2.imshow("Original, image")
+cv2.imshow("Original", image)
 
 r = 50.0 / image.shape[0]
 dim = (int(image.shape[1] * r), 50)
@@ -17,4 +17,6 @@ resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 cv2.imshow("Resized (Height)", resized)
 cv2.waitKey(0)
 
-
+resized = imutils.resize(image, width = 400)
+cv2.imshow("Resized with function", resized)
+cv2.waitKey(0)
