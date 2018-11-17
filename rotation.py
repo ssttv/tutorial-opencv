a6d43f2a08d15d5b7c79697573e800fd579b346c
Sergey Titov <sstmail@mail.ru>
2018-11-17 04:18:17 +0300

    feat: rotate image with easy access functions

--- rotation.py
+++ rotation.py
@@ -7,6 +7,10 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help = "Path to the image")
 args = vars(ap.parse_args())
 
-image = cv2.imshow(args["image"])
+image = cv2.imread(args["image"])
 cv2.imshow("Original", image)
+cv2.waitKey(0)
 
+rotated = imutils.rotate(image, 45, scale=1.0)
+cv2.imshow("Rotated by 45 degrees", rotated)
+cv2.waitKey(0)
