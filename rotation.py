09d928a08c77ca48d3ce7447c073d4561bc48fa9
Sergey Titov <sstmail@mail.ru>
2018-11-17 03:36:02 +0300

    feat: start image rotation

---
+++ rotation.py
@@ -0,0 +1,11 @@
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image - cv2.imshow(args["image"])
+cv2.imshow("Original", image)
