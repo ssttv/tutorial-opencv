e03ed9cbc6a583a71223df2300bb872b702c31ef
Sergey Titov <sstmail@mail.ru>
2018-11-19 14:35:42 +0300

    feat: draw baseline figures with cv2

---
+++ bitwise.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+rectangle  = np.zeros((300, 300), dtype = "uint8")
+cv2.rectangle(rectangle, (25, 25), (275, 275), 255, -1)
+cv2.imshow("Rectangle", rectangle)
+
+circle = np.zeros((300, 300), dtype = "uint8")
+cv2.circle(circle, (150, 150), 150, 255, -1)
+cv2.imshow("Circle", circle)
